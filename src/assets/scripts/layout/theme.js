0d4bfcb3e5f5f764c32c7237b1d3ad38e4ce4021
bryan-reed-88 <bryanreed@Commerce-MacBook-Pro.local>
2018-07-10 13:29:54 -0700

    Initial commit

---
+++ src/assets/scripts/layout/theme.js
@@ -0,0 +1,57 @@
+import "lazysizes/plugins/object-fit/ls.object-fit";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+import "lazysizes/plugins/rias/ls.rias";
+import "lazysizes/plugins/bgset/ls.bgset";
+import "lazysizes";
+import "lazysizes/plugins/respimg/ls.respimg";
+
+import "../../styles/theme.scss";
+import "../../styles/theme2.scss.liquid";
+
+import $ from "jquery";
+import { pageLinkFocus } from "@shopify/theme-a11y";
+import { cookiesEnabled } from "@shopify/theme-cart";
+import { wrapTable, wrapIframe } from "@shopify/theme-rte";
+
+import mobileNav from "../sections/mobile_nav";
+
+window.slate = window.slate || {};
+window.theme = window.theme || {};
+
+$(document).ready(() => {
+  const mobile_nav = new mobileNav();
+  // Common a11y fixes
+  if (window.location.hash !== "#") {
+    pageLinkFocus($(window.location.hash));
+  }
+
+  $(".in-page-link").on("click", evt => {
+    pageLinkFocus($(evt.currentTarget.hash));
+  });
+
+  // Target tables to make them scrollable
+  const tableSelectors = ".rte table";
+
+  wrapTable({
+    $tables: $(tableSelectors),
+    tableWrapperClass: "rte__table-wrapper"
+  });
+
+  // Target iframes to make them responsive
+  const iframeSelectors =
+    '.rte iframe[src*="youtube.com/embed"],' +
+    '.rte iframe[src*="player.vimeo"]';
+
+  wrapIframe({
+    $iframes: $(iframeSelectors),
+    iframeWrapperClass: "rte__video-wrapper"
+  });
+
+  // Apply a specific class to the html element for browser support of cookies.
+  if (cookiesEnabled()) {
+    document.documentElement.className = document.documentElement.className.replace(
+      "supports-no-cookies",
+      "supports-cookies"
+    );
+  }
+});
---
+++ src/assets/scripts/sections/mobile_nav.js
@@ -0,0 +1,117 @@
+import $ from "jquery";
+class mobileNav {
+  constructor() {
+    //Selectors
+    this.openMenuTrigger = ".main-header__mobile-trigger";
+    this.closeMenuTrigger = ".mobile-nav-close";
+    this.menuContainer = ".main-nav";
+    this.menuBackground = ".mobile-nav-background";
+    this.dropdownsLeft = ".layout-left .mega-menu>a, .layout-left .dropdown>a";
+    this.dropdownsDown = ".layout-down .mega-menu>a, .layout-down .dropdown>a";
+    this.backBtn = ".side-back";
+
+    //Start listening for events
+    this.events();
+  }
+  events() {
+    $(document).on("click", this.openMenuTrigger, this.openMenu.bind(this));
+    $(document).on("click", this.closeMenuTrigger, this.closeMenu.bind(this));
+    $(document).on(
+      "click",
+      this.dropdownsLeft,
+      this.toggleSubNavLeft.bind(this)
+    );
+    $(document).on(
+      "click",
+      this.dropdownsDown,
+      this.toggleSubNavDown.bind(this)
+    );
+    $(document).on("click", this.backBtn, this.toggleBackNav.bind(this));
+    // this.openMenuTrigger.click(this.openMenu.bind(this));
+    // this.closeMenuTrigger.click(this.closeMenu.bind(this));
+  }
+  //   Open the mobile menu
+  openMenu() {
+    $(this.openMenuTrigger).toggleClass("mobile-open");
+    $(document)
+      .find(this.menuContainer)
+      .toggleClass("mobile-open");
+    $(document)
+      .find(this.closeMenuTrigger)
+      .addClass("mobile-open");
+    //Add close button only to left layout
+    if ($(".main-nav.layout-left").length) {
+      $(".main-nav ul").append('<li class="mobile-nav-close">&#10005;</li>');
+    }
+    // this.toggle.toggleClass("mobile-open");
+    // $(document)
+    //   .find(this.menuBackground)
+    //   .addClass("mobile-open");
+  }
+  //Close the mobile menu
+  closeMenu() {
+    $(this.openMenuTrigger).removeClass("mobile-open");
+    $(document)
+      .find(this.menuContainer)
+      .removeClass("mobile-open");
+    $(this.closeMenuTrigger).remove();
+    //   $(document)
+    //     .find(this.menuBackground)
+    //     .removeClass("mobile-open");
+    //   $(document)
+    //     .find(this.closeMenuTrigger)
+    //     .removeClass("mobile-open");
+  }
+  toggleBackNav(e) {
+    e.preventDefault();
+    $(e.target)
+      .closest("li.open")
+      .removeClass("open");
+    $(e.target)
+      .closest(".sub-open")
+      .removeClass("sub-open");
+    $(e.target)
+      .parent()
+      .remove();
+  }
+  toggleSubNavDown(e) {
+    e.preventDefault();
+    //Toggle class on target
+    if (
+      $(e.target)
+        .parent("li")
+        .find(".mega-menu__container").length
+    ) {
+      let str =
+        '<div class="mega-menu__container__col"><div class="mega-menu__container__col__text"><a class="column_header side-back" href="">&lt; Back</a></div></div>';
+      $(e.target)
+        .parent("li")
+        .find(".mega-menu__container")
+        .prepend(str);
+    } else {
+      $(e.target)
+        .parent("li")
+        .find("ul")
+        .prepend('<li class="side-back"><a href="">&lt; Back</a></li>');
+    }
+    $(e.target)
+      .parent("li")
+      .toggleClass("open");
+    $(e.target)
+      .parent("li")
+      .parent("ul")
+      .toggleClass("sub-open");
+  }
+  toggleSubNavLeft(e) {
+    e.preventDefault();
+    //Toggle class on target
+    $(e.target)
+      .parent("li")
+      .toggleClass("open");
+    $(e.target)
+      .parent("li")
+      .parent("ul")
+      .toggleClass("sub-open");
+  }
+}
+export default mobileNav;
